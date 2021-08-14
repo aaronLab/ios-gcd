099eea7106560d910468d025ae0074c1134c93f6
Aaron Lee <aaronlab.net@gmail.com>
2021-08-14 20:31:21 +0900

    add timecheck

--- GCD Example.playground/Sources/timeCheck.swift
+++ GCD Example.playground/Sources/timeCheck.swift
@@ -1,2 +1,16 @@
 import Foundation
 
+@discardableResult
+public func timeCheck(_ block: () -> Void) -> TimeInterval {
+    let start = Date()
+    
+    block()
+    
+    let end = Date().timeIntervalSince(start)
+    
+    print("🕐 The whole progress time interval: \(end)")
+    
+    print("==============================")
+    
+    return end
+}
