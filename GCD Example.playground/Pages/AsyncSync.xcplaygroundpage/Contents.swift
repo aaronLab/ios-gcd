407b2c8ad316b5e0a259d0bbec33a85fc5a2d533
Aaron Lee <aaronlab.net@gmail.com>
2021-08-14 20:31:28 +0900

    edit test cases

--- GCD Example.playground/Pages/AsyncSync.xcplaygroundpage/Contents.swift
+++ GCD Example.playground/Pages/AsyncSync.xcplaygroundpage/Contents.swift
@@ -1,63 +1,74 @@
-//: [Previous](@previous)
-
-
 import Foundation
 
 let queue = DispatchQueue.global()
 
-func task1(type: String) {
-    print("\(type) Task 1 Start")
+func task1() {
+    print("Task 1 Start")
     
     sleep(1)
     
-    print("\(type) Task 1 Finished")
+    print("Task 1 Finished")
 }
 
-func task2(type: String) {
-    print("\(type) Task 2 Start")
+func task2() {
+    print("Task 2 Start")
+    
+    sleep(4)
+    
+    print("Task 2 Finished")
+}
+
+func task3() {
+    print("Task 3 Start")
+    
+    print("Task 3 Finished")
+}
+
+func task4() {
+    print("Task 4 Start")
     
     sleep(2)
     
-    print("\(type) Task 2 Finished")
+    print("Task 4 Finished")
 }
 
-func task3(type: String) {
-    print("\(type) Task 3 Start")
+//timeCheck {
+//
+//    queue.async {  // another thread
+//        task1()
+//    }
+//
+//    queue.async {  // another thread
+//        task2()
+//    }
+//
+//    queue.async {  // another thread
+//        task3()
+//    }
+//    queue.async {  // another thread
+//        task4()
+//    }
+//
+//}
+
+//task1()  // thread 1
+//task2()  // thread 1
+//task3()  // thread 1
+//task4()  // thread 1
+
+timeCheck {
+    queue.sync {  // another thread
+        task1()
+    }
     
-    print("\(type) Task  Finished")
+    queue.sync {  // another thread
+        task2()
+    }
+    
+    queue.sync {  // another thread
+        task3()
+    }
+    queue.sync {  // another thread
+        task4()
+    }
 }
-
-/*
- Async
- */
-
-queue.async {
-    task1(type: "Async")
-}
-
-queue.async {
-    task2(type: "Async")
-}
-
-queue.async {
-    task3(type: "Async")
-}
-
-/*
- Sync
- */
-
-queue.sync {
-    task1(type: "Sync")
-}
-
-queue.sync {
-    task2(type: "Sync")
-}
-
-queue.sync {
-    task3(type: "Sync")
-}
-
-
-//: [Next](@next)
