44b355041cc8883d9afa67025c68cc821e3b2299
Aaron Lee <aaronlab.net@gmail.com>
2021-10-04 14:39:46 +0900

    thread safe

--- LazyVar.playground/Contents.swift
+++ LazyVar.playground/Contents.swift
@@ -10,10 +10,41 @@ let queue = DispatchQueue(label: "queue", attributes: [.initiallyInactive, .conc
 let group = DispatchGroup()
 let instance = SharedClass()
 
+//for i in 0..<10 {
+//    group.enter()
+//    queue.async(group: group) {
+//        print("id: \(i), var: \(instance.lazyTest)")
+//        group.leave()
+//    }
+//}
+//
+//group.notify(queue: DispatchQueue.global()) {
+//    print("Done")
+//}
+//
+//queue.activate()
+//group.wait()
+
+class SyncSharedClass {
+    let queue = DispatchQueue(label: "serial")
+    
+    lazy var lazyTest = {
+        return Int.random(in: 0..<99)
+    }()
+    
+    var readVar: Int {
+        queue.sync {
+            return lazyTest
+        }
+    }
+}
+
+let instance2 = SyncSharedClass()
+
 for i in 0..<10 {
     group.enter()
     queue.async(group: group) {
-        print("id: \(i), var: \(instance.lazyTest)")
+        print("id: \(i), var: \(instance2.readVar)")
         group.leave()
     }
 }
@@ -24,3 +55,4 @@ group.notify(queue: DispatchQueue.global()) {
 
 queue.activate()
 group.wait()
+
