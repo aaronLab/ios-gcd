acb166ec72e8ae4aafe9c9c5dc09b53623031120
Aaron Lee <aaronlab.net@gmail.com>
2021-12-12 21:35:08 +0900

    DispatchGroup

---
+++ playgrounds/DispatchGroup.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+let group = DispatchGroup()
+let queue = DispatchQueue.global(qos: .userInitiated)
+
+queue.async(group: group) {
+    print("Start job 1")
+    Thread.sleep(until: Date().addingTimeInterval(10))
+    print("End job 1")
+}
+
+queue.async(group: group) {
+    print("Start job 2")
+    Thread.sleep(until: Date().addingTimeInterval(2))
+    print("End job 2")
+}
+
+if group.wait(timeout: .now() + 5) == .timedOut {
+    print("Failed...")
+} else {
+    print("All the jobs complted")
+}
